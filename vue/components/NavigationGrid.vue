4eb43add3248690e44633b336ee1827add6ed31b
jamesread <contact@jread.com>
2026-01-08 12:10:44 +0000

    fix: Wider buttons on NavigationGrid

--- vue/components/NavigationGrid.vue
+++ vue/components/NavigationGrid.vue
@@ -124,7 +124,7 @@ function handleLinkClick(link) {
 
 .navigation-grid {
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+	grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 	gap: 1rem;
 	padding: 1rem 0;
 }
